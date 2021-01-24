1a12cde260cf05831328307dd02d75e199140899
Keith Bennett <keithrbennett@gmail.com>
2021-01-24 18:15:28 -0500

    Refactor. Beware of `sleep`, it seems to obliterate benchmarks (they report as zero).

--- ractor-file-strings-test.rb
+++ ractor-file-strings-test.rb
@@ -23,8 +23,6 @@ class RactorFileParser
   end
 
   def parse(filespec)
-    # e.g.: ractor_9     casa/app/controllers/case_court_reports_controller.rb
-    printf("%-12s %s\n", name, filespec)
     file_lines(filespec).each_with_object(Set.new) do |line, file_words|
       line_words(line).each { |word| file_words << word }
     end
@@ -39,6 +37,7 @@ class RactorFileParser
   end
 
   private def file_lines(filespec)
+    return if filespec.empty?
     command = "strings #{Shellwords.escape(filespec)}"
     strip_punctuation(`#{command}`).split("\n")
   end
@@ -54,8 +53,12 @@ class Main
   BASEDIR =  ARGV[0] || '.'
   FILEMASK = ARGV[1]
 
+  attr_reader :ractor_count
+
   def call
     check_arg_count
+    init_ractor_count
+    ractor_count # pre-fill
     ractors = create_and_populate_ractors
     all_words = nil
     benchmark = Benchmark.measure { all_words = collate_ractor_results(ractors) }
@@ -63,19 +66,16 @@ class Main
   end
 
 
-  private def ractor_count
-    unless @ractor_count
-      specified_as_env_var = !!ENV['RACTOR_COUNT']
-      @ractor_count = specified_as_env_var ? ENV['RACTOR_COUNT'].to_i : Etc.nprocessors
+  private def init_ractor_count
+    specified_as_env_var = !!ENV['RACTOR_COUNT']
+    @ractor_count = specified_as_env_var ? ENV['RACTOR_COUNT'].to_i : Etc.nprocessors
 
-      raise "Ractor count must > 0." unless @ractor_count > 0
+    raise "Ractor count must > 0." unless @ractor_count > 0
 
-      unless specified_as_env_var
-        puts "Using the number of CPU's (#{@ractor_count}) as the number of ractors."
-        puts "You can also optionally specify the number of ractors to use with the environment variable RACTOR_COUNT."
-      end
+    unless specified_as_env_var
+      puts "Using the number of CPU's (#{@ractor_count}) as the number of ractors.\n" \
+           + "You can also optionally specify the number of ractors to use with the environment variable RACTOR_COUNT."
     end
-    @ractor_count
   end
 
 
@@ -108,6 +108,28 @@ class Main
 
   private def create_parser_ractor(seq_no)
     Ractor.new(name: "ractor_#{seq_no}") do
+
+      filespec_valid = ->(filespec) do
+        valid = true
+        if filespec.is_a?(Array)
+          puts "\n\n\n!!!!\nRactor received a message that contained the list of all filespecs, instead of a single filespec."
+          puts "This was not sent via the filespec yielder. Why? Skipping it...\n!!!!\n\n"
+          valid = false
+        end
+
+        valid = false if filespec.to_s == ''
+        valid
+      end
+
+      report_completion = ->(log, start_time) do
+        # e.g.: Ractor ractor_16    duration (secs): 18.76906
+        message = sprintf("Ractor %-12s duration (secs): %.5f\n", name, (Time.now - start_time).round(5))
+        puts message
+        log.puts message
+      end
+
+      # e.g.:    46.33613            +45.68088  Completed processing casa/app/documents/templates/report_template_non_transition.docx
+
       File.open("#{name}.log", 'w') do |log|
         found_words = Set.new
         dictionary_words = Ractor.receive
@@ -117,27 +139,16 @@ class Main
 
         loop do
           filespec = yielder.take
+          next unless filespec_valid.(filespec)
 
           # e.g.:      0.00001   Received casa/app/models/followup.rb for processing.
           log.printf("%12.5f  Received %s for processing.\n", (Time.now - start_time).round(5), filespec)
 
-          if filespec.is_a?(Array)
-            puts "\n\n\n!!!!\nRactor received a message that contained the list of all filespecs, instead of a single filespec."
-            puts "This was not sent via the filespec yielder. Why? Skipping it...\n!!!!\n\n"
-            next
-          end
           file_start_time = Time.now
           found_words |= parser.parse(filespec)
-
-          # e.g.:    46.33613                                        +45.68088  Completed processing casa/app/documents/templates/report_template_non_transition.docx
           log.printf("%12.5f%12s+%8.5f Completed processing %s\n", (Time.now - start_time).round(5), '', (Time.now - file_start_time).round(5), filespec)
         end
-
-        # e.g.: Ractor ractor_16    duration (secs): 18.76906
-        message = sprintf("Ractor %-12s duration (secs): %.5f", name, (Time.now - start_time).round(5))
-        puts message
-        log.puts message
-
+        report_completion.(log, start_time)
         found_words
       end
     end
@@ -146,21 +157,38 @@ class Main
 
   private def create_filespec_yielding_ractor(all_filespecs)
     ractor = Ractor.new(name: 'FilespecYielder') do
-      File.open('filespec_yielder_ractor.log', 'w') do |log|
-        filespecs = Ractor.receive
-        filespecs.each do |filespec|
-          Ractor.yield(filespec)
-          log.puts filespec
+      filespecs = Ractor.receive
+      report_interval_secs = 1
+      time_to_report_progress = Time.now - 100 # some time in the past
+      file_count = filespecs.size
+      puts # make some vertical whitespace to offset the ractor warning
+
+      report_progress = ->(file_num) do
+        if Time.now > time_to_report_progress
+          percent_complete = (100.0 * file_num / file_count).round(2)
+          message = sprintf "%05.2f%% complete [%6d / %6d]", percent_complete, file_num, file_count
+          print("\e[G")
+          print message
+          time_to_report_progress = Time.now + report_interval_secs
         end
       end
+
+      File.open('filespec_yielder_ractor.log', 'w') do |log|
+        report_progress.(0)
+        filespecs.each_with_index do |filespec, file_num|
+          Ractor.yield(filespec)
+          report_progress.(file_num)
+        end
+        puts "\nFinished sending filespecs to ractors. They may take a while to finish processing.\n\n"
+      end
     end
+
     ractor.send(all_filespecs)
     ractor
   end
 
 
   private def create_and_populate_ractors
-    puts "Creating #{ractor_count} ractor(s)."
     filespec_yielder = create_filespec_yielding_ractor(find_all_filespecs)
     dictionary_words = File.readlines('/usr/share/dict/words').map(&:chomp).map(&:downcase).sort
     ractors = (0...ractor_count).map { |n| create_parser_ractor(n) }
